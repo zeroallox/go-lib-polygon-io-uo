bd44af9792bf44a9a21162aab323af7ec42a8dfb
Andrew <andrew@dkai.dk>
2021-08-16 15:01:57 -0400

    Update Common.go

--- polysocket/Common.go
+++ polysocket/Common.go
@@ -3,6 +3,8 @@ package polysocket
 import jsoniter "github.com/json-iterator/go"
 
 var json = jsoniter.Config{
+	SortMapKeys:                   false,
 	EscapeHTML:                    false,
+	ValidateJsonRawMessage:        false,
 	ObjectFieldMustBeSimpleString: true,
 }.Froze()
