ceecc0126676460ce5451cf6cea6c0baf5ce578f
Andrew <andrew@dkai.dk>
2021-10-29 07:52:02 -0400

    Updates
    polypsv: Added GetLineCount()

--- polypsv/Constants.go
+++ polypsv/Constants.go
@@ -1,14 +1,15 @@
 package polypsv
 
 import (
-	"github.com/zeroallox/go-lib-polygon-io-uo/polyconst"
-	"time"
+    "github.com/zeroallox/go-lib-polygon-io-uo/polyconst"
+    "time"
 )
 
 var pipeCharBytes = []byte("|")
 var pipeCharByte byte = '|'
 var newLineCharBytes = []byte("\n")
+var newLineCharByte byte = '\n'
 
 var localeTimeZoneMap = map[polyconst.Locale]*time.Location{
-	polyconst.LOC_USA: polyconst.NYCTime,
+    polyconst.LOC_USA: polyconst.NYCTime,
 }
--- polypsv/Utils.go
+++ polypsv/Utils.go
@@ -1,19 +1,22 @@
 package polypsv
 
 import (
-	"os"
-	"path/filepath"
+    "bytes"
+    "github.com/klauspost/compress/gzip"
+    "io"
+    "os"
+    "path/filepath"
 )
 
 // CreateLocalPSVFile creates the correct directory structure within baseDir
 // and returns a handle to an empty io.File.
 func CreateLocalPSVFile(baseDir string, file *FileInfo) (*os.File, error) {
-	var err = os.MkdirAll(filepath.Join(baseDir, MakeDirPath(file)), os.ModePerm)
-	if err != nil {
-		return nil, err
-	}
+    var err = os.MkdirAll(filepath.Join(baseDir, MakeDirPath(file)), os.ModePerm)
+    if err != nil {
+        return nil, err
+    }
 
-	return os.Create(filepath.Join(baseDir, MakeABSFilePath(file)))
+    return os.Create(filepath.Join(baseDir, MakeABSFilePath(file)))
 }
 
 // OpenLocalPSVFile returns a handle to file within baseDir.
@@ -22,5 +25,52 @@ func CreateLocalPSVFile(baseDir string, file *FileInfo) (*os.File, error) {
 //  we generate the correct path where the file should be and returns a handle
 //  to it.
 func OpenLocalPSVFile(baseDir string, file *FileInfo) (*os.File, error) {
-	return os.Open(filepath.Join(baseDir, MakeABSFilePath(file)))
+    return os.Open(filepath.Join(baseDir, MakeABSFilePath(file)))
+}
+
+// GetLineCount returns the number of lines in the PSV file including the header.
+func GetLineCount(baseDir string, file *FileInfo) (uint, error) {
+
+    hFile, err := OpenLocalPSVFile(baseDir, file)
+    if err != nil {
+        return 0, err
+    }
+    defer func() {
+        _ = hFile.Close()
+    }()
+
+    var reader io.Reader
+    if file.Compressed() == true {
+        reader, err = gzip.NewReader(hFile)
+        if err != nil {
+            return 0, err
+        }
+    } else {
+        reader = hFile
+    }
+
+    count, err := countLines(reader)
+    if err != nil {
+        return 0, err
+    }
+
+    return uint(count), nil
+}
+
+// Stolen from: https://stackoverflow.com/questions/24562942
+func countLines(r io.Reader) (int, error) {
+    var buf = make([]byte, 32*1024)
+    var count = 0
+
+    for {
+        c, err := r.Read(buf)
+        count += bytes.Count(buf[:c], newLineCharBytes)
+
+        switch {
+        case err == io.EOF:
+            return count, nil
+        case err != nil:
+            return count, err
+        }
+    }
 }
