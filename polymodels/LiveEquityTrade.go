e6752f36f55d5c130b3b8ad511d0f3ac6a0a571f
Andrew <andrew@dkai.dk>
2022-04-28 09:26:44 -0400

    Update LiveEquityTrade.go

--- polymodels/LiveEquityTrade.go
+++ polymodels/LiveEquityTrade.go
@@ -4,11 +4,12 @@ type LiveEquityTrade struct {
     baseModel
 
     Ticker     string  `json:"sym"`
-    Exchange   int     `json:"x"`
     TradeID    string  `json:"i"`
-    Tape       int     `json:"z"`
+    Exchange   int     `json:"x"`
     Price      float64 `json:"p"`
     Volume     int     `json:"s"`
     Conditions []int   `json:"c"`
     Timestamp  int64   `json:"t"`
+    Sequence   int     `json:"q"`
+    Tape       int     `json:"z"`
 }
